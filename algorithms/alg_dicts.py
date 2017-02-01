e5e9aa4cc661b08d1bba97d812b11736bfb82eac
Jacob Scott <jscott12009@gmail.com>
2017-02-01 14:21:36 -0800

    Renamed misc.py and added oll_dict

--- algorithms/alg_dicts.py
+++ algorithms/alg_dicts.py
@@ -1,4 +1,3 @@
-
 u, l, f = 'turn_up', 'turn_left', 'turn_front'
 r, b, d = 'turn_right', 'turn_back', 'turn_down'
 m = 'turn_xmiddle'
@@ -20,3 +19,20 @@ turn_dict = {'U' : [u, 1, False], 'T' : [u, -1, False], '!' : [u, 2, False],
              'x' : [x, True, False], 'X' : [x, False, False], '8' : [x, False, True],
              'y' : [y, True, False], 'Y' : [y, False, False], '9' : [y, False, True],
              'z' : [z, True, False], 'Z' : [z, False, False], '0' : [z, False, True]}
+
+
+oll_dict =  {'01010101' : 'flip_tshirt',        '01100011' : 'a_moustache',
+             '10010001' : 'squeegee_agun',      '10011100' : 'fishsalad_mountedfish',
+             '01010111' : 'a_sune',             '00010011' : 'gun_asqueegee',
+             '00110100' : 'upstairs_lightning', '00011100' : 'leftysquare_rightysquare',
+             '01001001' : 'a_kite',             '10110100' : 'aspotcham_dalmation', 
+             '01111000' : 'couch_apee',         '00011011' : 'city_cingheadlights',  
+             '10100000' : 'bunny_crown',        '00001000' : 'a_mouse',
+             '00000000' : 'blank_zamboni',      '01111101' : 'chameleon_headlights', 
+             '00111001' : 'key_suitup',         '10000101' : 'alightning_downstairs',
+             '00111100' : 'acouch_pee',         '01101001' : 'spotcham_adalmation',
+             '01000001' : 'a_breakneck_a_fryingpan_rfsqueezy_rbsqueezy',
+             '01000100' : 'highway_ricecooker_streetlights_ant',
+             '10001000' : 'slash', '01110111' : 'diagonal', '00110011' : 'fung',
+             '10011001' : 'afung', '10101010' : 'checkers', '10111110' : 'stealth',
+             '10111011' : 'brick'}
