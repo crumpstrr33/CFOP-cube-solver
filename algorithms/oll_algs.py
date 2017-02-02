9c8ed91c3c47cdcd31603ce724837251f467756b
Jacob Scott <jscott12009@gmail.com>
2017-02-02 15:13:39 -0800

    Simplified OLLCases by using unique identifiers for oll cases

--- algorithms/oll_algs.py
+++ algorithms/oll_algs.py
@@ -5,118 +5,78 @@ Names from https://www.speedsolving.com/wiki/index.php/OLL.
 from algorithms.tools import alg_to_code
 
 '''
-No edges flipped correctly
+(R U R' U') Triggers
 '''
-blank     = alg_to_code("RU2R2FRF'U2R'FRF'")                 # Case 24
-zamboni   = alg_to_code("FRUR'U'F'fRUR'U'f'")                # Case 12
-amouse    = alg_to_code("y'" + "fRUR'U'f'U'FRUR'U'F'" + "y") # Case 11
-mouse     = alg_to_code("fRUR'U'f'UFRUR'U'F'")               # Case 10
-slash     = alg_to_code("RUR'UR'FRF'U2R'FRF'")               # Case 31
-crown     = alg_to_code("y2" + "FRUR'UF'y'U2R'FRF'" + "y2")  # Case 32
-bunny     = alg_to_code("MURUR'U'M'R'FRF'")                  # Case 26
+case_01 = alg_to_code("FRUR'U'F'")              # Suitup
+case_02 = alg_to_code("FRUR'U'RUR'U'F'")        # Breakneck
+case_03 = alg_to_code("yR'U'RU'R'URU'R'U2R")    # Flip
+case_04 = alg_to_code("fRUR'U'f'")              # P
+case_05 = alg_to_code("fRUR'U'RUR'U'f'")        # Ant
+case_06 = alg_to_code("fL'U'LUf'")              # AntiP
+case_07 = alg_to_code("F'L'U'LUL'U'LUF")        # Antibreakneck
+case_08 = alg_to_code("FRUR'U'F'UFRUR'U'F'")    # Upstairs
+case_09 = alg_to_code("yrUR'UR'FRF'RU2r'")      # Downstairs
+case_10 = alg_to_code("fRUR'U'f'UFRUR'U'F'")    # Moue
+case_11 = alg_to_code("fRUR'U'f'U'FRUR'U'F'")   # Antimouse
+case_12 = alg_to_code("FRUR'U'F'fRUR'U'f'")     # Zamboni
+case_13 = alg_to_code("RU2R2U'R2U'R2U2R")       # Tshirt
+case_14 = alg_to_code("rUr'RUR'U'rU'r'")        # Antisqueegee
+case_15 = alg_to_code("l'U'lL'U'LUl'Ul")        # Squeegee
+case_16 = alg_to_code("R'FRUR'U'F'UR")          # Antifung
+case_17 = alg_to_code("RUR'U'M'URU'r'")         # Brick
+case_18 = alg_to_code("MURUR'U'M2URU'r'")       # Checkers
+case_19 = alg_to_code("FRUR'U'RF'rUR'U'r'")     # Streetlights
 
 '''
-All edges flipped correctly
+(R' F R F') Triggers
 '''
-sune       = alg_to_code("RUR'URU2R'")                    # Case 35
-asune      = alg_to_code("y2" + "RU2R'U'RU'R'" + "y2")    # Case 36
-flip       = alg_to_code("yR'U'RU'R'URU'R'U2R")           # Case  3
-tshirt     = alg_to_code("RU2R2U'R2U'R2U2R")              # Case 13
-headlights = alg_to_code("y'" + "R2DR'U2RD'R'U2R'" + "y") # Case 48
-chameleon  = alg_to_code("rUR'U'r'FRF'")                  # Case 21
-diagonal   = alg_to_code("F'rUR'U'r'FR")                  # Case 22
+case_20 = alg_to_code("RUR'U'R'FRF'")           # Key
+case_21 = alg_to_code("rUR'U'r'FRF'")           # Chameleon
+case_22 = alg_to_code("F'rUR'U'r'FR")           # Diagonal
+case_23 = alg_to_code("R'U'R'FRF'UR")           # Seein' Headlights
+case_24 = alg_to_code("RU2R2FRF'U2R'FRF'")      # Blank
+case_25 = alg_to_code("RU2R2FRF'RU2R'")         # Fishsalad
+case_26 = alg_to_code("MURUR'U'M'R'FRF'")       # Bunny
+case_27 = alg_to_code("R'FR'F'R2U2yR'FRF'")     # Rightback Squeezy
 
 '''
-P shapes
+(R U R' U) Triggers
 '''
-couch  = alg_to_code("R'U'FURU'R'F'R")          # Case 51
-acouch = alg_to_code("RdL'd'R'URBR'")           # Case 50
-pee    = alg_to_code("fRUR'U'f'")               # Case  4
-apee   = alg_to_code("y2" + "fL'U'LUf'" + "y2") # Case  6
+case_28 = alg_to_code("RUR'URU'R'U'R'FRF'")     # Moustache
+case_29 = alg_to_code("L'U'LU'L'ULULF'L'F")     # Antimoustache
+case_30 = alg_to_code("RUR'URd'RU'R'F'")        # Ricecooker
+case_31 = alg_to_code("RUR'UR'FRF'U2R'FRF'")    # Slash
+case_32 = alg_to_code("FRUR'UF'y'U2R'FRF'")     # Crown
+case_33 = alg_to_code("r'U2RUR'Ur")             # Lefty Square
+case_34 = alg_to_code("rUR'URU2r'")             # Lightning
 
 '''
-W shapes
+Sune-like algorithms
 '''
-moustache  = alg_to_code("RUR'URU'R'U'R'FRF'")              # Case 28
-amoustache = alg_to_code("y" + "L'U'LU'L'ULULF'L'F" + "y'") # Case 29
+case_35 = alg_to_code("RUR'URU2R'")             # Sune
+case_36 = alg_to_code("RU2R'U'RU'R'")           # Antisune
+case_37 = alg_to_code("R'FRF'R'FRF'RUR'U'RUR'") # Antidalmation
+case_38 = alg_to_code("RUR'URU2R'FRUR'U'F'")    # Dalmation
 
 '''
-L Shapes
+Misc and Awkward algorithms
 '''
-breakneck     = alg_to_code("FRUR'U'RUR'U'F'")                 # Case  2
-abreakneck    = alg_to_code("y'" + "F'L'U'LUL'U'LUF" + "y")    # Case  7
-fryingpan     = alg_to_code("l'U'LU'L'ULU'L'U2l")              # Case 40
-afryingpan    = alg_to_code("y'" + "rUR'URU'R'URU2r'" + "y")   # Case 39
-rbsqueezy     = alg_to_code("y'" + "R'FR'F'R2U2yR'FRF'" + "y") # Case 27
-rfsqueezy     = alg_to_code("R'FR2B'R2F'R2BR'")                # Case 53
-
-'''
-C Shapes
-'''
-city           = alg_to_code("RUR2U'R'FRURU'F'")          # Case 45
-cingheadlights = alg_to_code("y'" + "R'U'R'FRF'UR" + "y") # Case 23
-
-'''
-T Shapes
-'''
-key    = alg_to_code("RUR'U'R'FRF'") # Case 20
-suitup = alg_to_code("FRUR'U'F'")    # Case  1
-
-'''
-I Shapes
-'''
-highway      = alg_to_code("R'U2R2UR'URU2x'U'R'U")            # Case 49
-streetlights = alg_to_code("y'" + "FRUR'U'RF'rUR'U'r'" + "y") # Case 19
-ant          = alg_to_code("y' " + "fRUR'U'RUR'U'f'" + "y")   # Case  5
-ricecooker   = alg_to_code("RUR'URd'RU'R'F'")                 # Case 30
-
-'''
-Square shapes
-'''
-leftysquare  = alg_to_code("r'U2RUR'Ur")                # Case 33
-rightysquare = alg_to_code("y" + "rU2R'U'RU'r'" + "y'") # Case 41
-
-'''
-Big lightning bolt shapes
-'''
-fung  = alg_to_code("RB'R'U'RUBU'R'") # Case 52
-afung = alg_to_code("R'FRUR'U'F'UR")  # Case 16
-
-'''
-Small lightning bolt shapes
-'''
-lightning     = alg_to_code("y2" + "rUR'URU2r'" + "y2") # Case 34
-alightning    = alg_to_code("r'U'RU'R'U2r")             # Case 43
-downstairs    = alg_to_code("yrUR'UR'FRF'RU2r'")        # Case  9
-upstairs      = alg_to_code("FRUR'U'F'UFRUR'U'F'")      # Case  8
-
-'''
-Fish Shapes
-'''
-kite        = alg_to_code("RUR'U'R'FR2UR'U'F'")            # Case 56
-akite       = alg_to_code("y" + "RUR'yR'FRU'R'F'R" + "y'") # Case 57
-fishsalad   = alg_to_code("RU2R2FRF'RU2R'")                # Case 25
-mountedfish = alg_to_code("y2" + "FRU'R'U'RUR'F'" + "y2")  # Case 42
-
-'''
-Knight move shapes
-'''
-gun       = alg_to_code("FURU'R2F'RURU'R'")                # Case 46
-agun      = alg_to_code("y2" + "R'FRUR'F'Ry'RU'R'" + "y2") # Case 47
-squeegee  = alg_to_code("l'U'lL'U'LUl'Ul")                 # Case 15
-asqueegee = alg_to_code("y2" + "rUr'RUR'U'rU'r'" + "y2")   # Case 14
-
-'''
-Awkward shapes
-'''
-spotcham   = alg_to_code("RUR'U'RU'R'F'U'FRUR'")              # Case 54
-aspotcham  = alg_to_code("R2UR'B'RU'R2URBR'")                 # Case 55
-dalmation  = alg_to_code("y2" + "RUR'URU2R'FRUR'U'F'" + "y2") # Case 38
-adalmation = alg_to_code("R'FRF'R'FRF'RUR'U'RUR'")            # Case 37
-
-'''
-All corners oriented
-'''
-stealth  = alg_to_code("M'UMU2M'UM")       # Case 44
-brick    = alg_to_code("RUR'U'M'URU'r'")   # Case 17
-checkers = alg_to_code("MURUR'U'M2URU'r'") # Case 18
+case_39 = alg_to_code("rUR'URU'R'URU2r'")       # Antifryingpan
+case_40 = alg_to_code("l'U'LU'L'ULU'L'U2l")     # Fryingpan
+case_41 = alg_to_code("rU2R'U'RU'r'")           # Righty Square
+case_42 = alg_to_code("FRU'R'U'RUR'F'")         # Mounted Fish
+case_43 = alg_to_code("r'U'RU'R'U2r")           # Antilightning
+case_44 = alg_to_code("M'UMU2M'UM")             # Stealth
+case_45 = alg_to_code("RUR2U'R'FRURU'F'")       # City
+case_46 = alg_to_code("FURU'R2F'RURU'R'")       # Gun
+case_47 = alg_to_code("R'FRUR'F'Ry'RU'R'")      # Antigun
+case_48 = alg_to_code("R2DR'U2RD'R'U2R'")       # Headlights
+case_49 = alg_to_code("R'U2R2UR'URU2x'U'R'U")   # Highway
+case_50 = alg_to_code("RdL'd'R'URBR'")          # Anticouch
+case_51 = alg_to_code("R'U'FURU'R'F'R")         # Couch
+case_52 = alg_to_code("RB'R'U'RUBU'R'")         # Fung
+case_53 = alg_to_code("R'FR2B'R2F'R2BR'")       # Rightfront Squeezy
+case_54 = alg_to_code("RUR'U'RU'R'F'U'FRUR'")   # Spotted Chameleon
+case_55 = alg_to_code("R2UR'B'RU'R2URBR'")      # Antispotted Chameleon
+case_56 = alg_to_code("RUR'U'R'FR2UR'U'F'")     # Kite
+case_57 = alg_to_code("RUR'yR'FRU'R'F'R")       # Antikite
