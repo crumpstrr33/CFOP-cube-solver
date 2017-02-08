c40a9767f3d3272333988e28d3d96271d1d45dda
Jacob Scott <jscott12009@gmail.com>
2017-02-07 23:16:30 -0800

    Tenatively the final product with comments and all

--- oll_pll.py
+++ oll_pll.py
@@ -14,7 +14,7 @@ class OLLCases():
     2 clockwise twists. For edges, it is either 0 (for correct orientation) or
     1 if incorrect. They are ordered starting in the top left and moving
     counterclockwise. In retrospect, that's inconsistent, but it works fine and
-    consistency in this context isn't necessary or useful.
+    consistency in this context isn't necssary. Plus, I use it in PLLCases().
 
     The identifier is found with _tf_hash, then the OLL algorithm is
     found. If it's not found, an exception is thrown.
@@ -33,6 +33,9 @@ class OLLCases():
 
 
     def _tf_hash(self, perm):
+        '''
+        Turns the permutation into a unique identifier for each OLL
+        '''
         ## The correct stickers, looking for where the top face sticker is
         tf_stickers  = str((perm[0][0] + perm[1][0] + perm[4][2]).index('w'))
         tf_stickers += str((perm[0][7] + perm[1][1]).index('w'))
@@ -47,6 +50,10 @@ class OLLCases():
 
 
     def _rotate(self, s, n):
+        '''
+        Rotates a string (in this case, it's rotating corner to corner and edge
+        to edge, so rotation to the string are done by factors of 2).
+        '''
         if (n % 2) != 0:
             raise Exception('An incorrect rotation amount (n) was entered: %d.' % n +
                             ' It must be a multiple of 2.')
@@ -57,19 +64,20 @@ class OLLCases():
 
 
     def _find_oll(self):
-        tf_stickers = self.tf_stickers
-
+        '''
+        Finds the correct OLL needed to solve the OLL case
+        '''
         ## Checks against each OLL in the dict
         for oll in oll_dict:
             rotation = 0
 
             ## Rotates the OLL to check if roation was wrong
             for _ in range(4):
-                if  tf_stickers == oll:
+                if  self.tf_stickers == oll:
                     return oll_dict[oll], rotation
                 else:
                     ## If rotation was wrong, try again
-                    self._rotate(tf_stickers, 2)
+                    self._rotate(self.tf_stickers, 2)
                     rotation += 1
 
 
@@ -79,7 +87,13 @@ class PLLCases():
     PLL is found using the unique identifiers in pll_dict. This PLL (written as
     'x_perm') can be found in pll_algs.py.
     
-
+    This unique identifier is found by recording the last layer stickers 
+    starting on the right face in the top left and move counterclockwise around
+    the top face getting a string 12 long. Then each char is converted to an int
+    according to which face is should be (e.g. if red is the left face and there
+    is a red sticker on the left face, then that sticker will be replaced with a
+    2). This gives a unique identifier as to how the stickers should be rotated
+    around the cube.
 
     The identifier is found with _ll_hash, then the PLL algorithm is
     found. If it's not found, an exception is thrown.
@@ -98,6 +112,9 @@ class PLLCases():
 
 
     def _ll_hash(self, perm):
+        '''
+        Turns the permutation into a unique identifier for each PLL
+        '''
         ll_stickers = ''.join(perm[1:5].astype('|S3').astype(str))
         centers = [side[-1] for side in perm[1:5]]
 
@@ -108,6 +125,11 @@ class PLLCases():
 
 
     def _rotate(self, s, n):
+        '''
+        Rotates a string and converts it to an int corresponding to its correct
+        side (in this case, it's rotating the last layer of the side
+        faces, so it rotates by a factor of 3).
+        '''
         if (n % 3) != 0:
             raise Exception('An incorrect rotation amount (n) was entered: %d.' % n +
                             ' It must a multiple of 3.')
@@ -121,11 +143,18 @@ class PLLCases():
 
 
     def _find_pll(self):
+        '''
+        Finds the correct PLL needed to solve the PLL case
+        '''
+        ## Checks against each PLL in the dict
         for pll in pll_dict:
             rotation = 0
+
+            ## Rotates the PLL to check if roation was wrong
             for _ in range(4):
                 if self.ll_stickers == pll:
                     return pll_dict[pll], rotation
                 else:
+                    ## If rotation was wrong, try again
                     self._rotate(self.ll_stickers, 3)
                     rotation += 1
