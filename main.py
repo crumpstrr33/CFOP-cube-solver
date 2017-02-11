b055d3f94d1cf94c8e14f37ae341ffd5c835feaa
Jacob Scott <jscott12009@gmail.com>
2017-02-11 14:17:34 -0800

    Runs Solver for OLL and PLL and prints out algorithm

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ DIR = 'C:\\Users\\Jacob\\Documents\\coding_stuff\\Python\\CFOP_solver_3x3'
 if DIR not in path:
     path.append(DIR)
 
-from oll import OllCases
-from cube import Cube
+from solver import Solver
 from algorithms.tools import code_to_alg
-from algorithms import oll_algs
 
-def main():
-    cube = Cube(['wgbbgrwo', 'owoooooo', 'gwrggggg', 'wwwrrrrr', 'rwbbbbbb', 'yyyyyyyy'])
-    top_face = ['w', 0]
+def test(do_print=False):
+    cube = Solver(['bwgggwbb', 'rwoooooo', 'wowggggg', 'owrrrrrr', 'wrwbbbbb', 'yyyyyyyy'])
+    if do_print: print(cube.perm, '\n')
 
-    oll = OllCases(cube.perm, top_face)
+    cube.solve_oll()
 
+    if do_print: print('Case', cube.oll[-2:])
+    if do_print: print(cube.perm)
+    if do_print: print(code_to_alg(cube.solving_alg))
 
-    print('Use this algorithm to complete OLL:', code_to_alg(getattr(oll_algs, oll.oll)))
-    print('But first, do this rotation:', oll.rotation)
+    cube.solve_pll()
 
+    if do_print: print('\n', cube.pll)
+    if do_print: print(cube.perm)
+
+    if do_print: print('\nTotal:', code_to_alg(cube.solving_alg))
 
 if __name__ == "__main__":
-    main()
+    test(True)
