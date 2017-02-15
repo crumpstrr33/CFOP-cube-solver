1fb9813a5358787d79244caf12fc6d5b048e0db5
Jacob Scott <jscott12009@gmail.com>
2017-02-14 22:37:24 -0800

    Revert "Abstracted rotation and turning methods"
    This reverts commit 32b1090a8b63c0987d627b773811925e4b476e0d.

--- cube.py
+++ cube.py
@@ -1,9 +1,10 @@
 import numpy as np
+from collections import deque
 from algorithms.alg_dicts import turn_dict
-from algorithms.tools import alg_to_code, opposite, face_to_rotation
+from algorithms.tools import alg_to_code
 
 
-class Cube:
+class Cube():
     '''
     Cube class which creates an object representing a 3x3 Rubik's cube in some
     specific permutation as described below.
@@ -65,12 +66,239 @@ class Cube:
 
 
     def _rotate(self, *sides, length):
-        '''
-        Rotates the sides of the cube by length.
-        '''
         for side in sides:
-            rotated_side = self.perm[side][-length:] + self.perm[side][:-length]
-            self.perm[side] = rotated_side
+            deque_side = deque(self.perm[side])
+            deque_side.rotate(length)
+            self.perm[side] = ''.join(deque_side)
+
+
+    def turn_up(self, ttype, dl=False):
+        '''
+        Turns the up face of the cube depending on the args.
+        
+        Parameters:
+        ttype - The turn type. For a clockwise turn, use 'cw' or 1. For a 
+                counterclockwise turn, use 'ccw' or -1. And for a double turn,
+                use 'dt' or 2. The number represents the number of clockwise
+                turns that will be applied
+        dl - (optional) Whether to perform a double turn
+        '''
+        if dl:
+            self.turn_down(ttype)
+            self.rotate_y(ttype == 1, ttype == 2)
+        else:
+            ## Check if correct ttype
+            turn = (ttype in [1, 'cw']) - (ttype in [-1, 'ccw']) + 2 * (ttype in [2, 'dt'])
+            if turn not in [-1, 1, 2]:
+                raise Exception('ttype = %s, turn amount is %d which is not allowed.'
+                                % (ttype, turn))
+    
+            layer = 3
+            perm = self.perm[1:5]
+            new_perm = []
+    
+            for n, side in enumerate(perm):
+                next_side = (n + turn) % 4
+                new_perm.append(perm[next_side][:layer] + perm[n][layer:])
+    
+            self.perm[[1, 2, 3, 4]] = new_perm
+            self._rotate(0, length=2 * turn)
+
+
+    def turn_left(self, ttype, dl=False):
+        '''
+        Turns the left face of the cube depending on the args.
+        
+        Parameters:
+        ttype - The turn type. For a clockwise turn, use 'cw' or 1. For a 
+                counterclockwise turn, use 'ccw' or -1. And for a double turn,
+                use 'dt' or 2. The number represents the number of clockwise
+                turns that will be applied
+        dl - (optional) Whether to perform a double layer turn
+        '''
+        if dl:
+            self.turn_right(ttype)
+            self.rotate_x(ttype == -1, ttype == 2)
+        else:
+            self.rotate_z()
+            self.turn_up(ttype, dl)
+            self.rotate_z(cw=False)
+
+
+    def turn_front(self, ttype, dl=False):
+        '''
+        Turns the front face of the cube depending on the args.
+        
+        Parameters:
+        ttype - The turn type. For a clockwise turn, use 'cw' or 1. For a 
+                counterclockwise turn, use 'ccw' or -1. And for a double turn,
+                use 'dt' or 2. The number represents the number of clockwise
+                turns that will be applied
+        dl - (optional) Whether to perform a double layer turn
+        '''
+        if dl:
+            self.turn_back(ttype)
+            self.rotate_z(ttype == 1, ttype == 2)
+        else:
+            self.rotate_x()
+            self.turn_up(ttype, dl)
+            self.rotate_x(cw=False)
+
+
+    def turn_right(self, ttype, dl=False):
+        '''
+        Turns the right face of the cube depending on the args.
+        
+        Parameters:
+        ttype - The turn type. For a clockwise turn, use 'cw' or 1. For a 
+                counterclockwise turn, use 'ccw' or -1. And for a double turn,
+                use 'dt' or 2. The number represents the number of clockwise
+                turns that will be applied
+        dl - (optional) Whether to perform a double layer turn
+        '''
+        if dl:
+            self.turn_left(ttype)
+            self.rotate_x(ttype == 1, ttype == 2)
+        else:
+            self.rotate_z(cw=False)
+            self.turn_up(ttype, dl)
+            self.rotate_z()
+
+
+    def turn_back(self, ttype, dl=False):
+        '''
+        Turns the back face of the cube depending on the args.
+        
+        Parameters:
+        ttype - The turn type. For a clockwise turn, use 'cw' or 1. For a 
+                counterclockwise turn, use 'ccw' or -1. And for a double turn,
+                use 'dt' or 2. The number represents the number of clockwise
+                turns that will be applied
+        dl - (optional) Whether to perform a double layer turn
+        '''
+        if dl:
+            self.turn_front(ttype)
+            self.rotate_z(ttype == -1, ttype == 2)
+        else:
+            self.rotate_x(cw=False)
+            self.turn_up(ttype, dl)
+            self.rotate_x()
+
+
+    def turn_down(self, ttype, dl=False):
+        '''
+        Turns the down face of the cube depending on the args.
+        
+        Parameters:
+        ttype - The turn type. For a clockwise turn, use 'cw' or 1. For a 
+                counterclockwise turn, use 'ccw' or -1. And for a double turn,
+                use 'dt' or 2. The number represents the number of clockwise
+                turns that will be applied
+        dl - (optional) Whether to perform a double layer turn
+        '''
+        if dl:
+            self.turn_up(ttype)
+            self.rotate_y(ttype == -1, ttype == 2)
+        else:
+            self.rotate_x(dt=True)
+            self.turn_up(ttype, dl)
+            self.rotate_x(dt=True)
+
+
+    def turn_middle(self, ttype, axis='m'):
+        '''
+        Turns the middle slice on the z axis of the cube depending on ttype.
+        
+        Parameters:
+        ttype - The turn type. For a clockwise turn, use 'cw' or 1. For a 
+                counterclockwise turn, use 'ccw' or -1. And for a double turn,
+                use 'dt' or 2. The number represents the number of clockwise
+                turns that will be applied
+        '''
+        if axis == 'm':
+            if ttype in [1, 'cw']:
+                self.turn_right('cw')
+                self.turn_left('ccw')
+                self.rotate_x(cw=False)
+            elif ttype in [-1, 'ccw']:
+                self.turn_right('ccw')
+                self.turn_left('cw')
+                self.rotate_x()
+            elif ttype in [2, 'dt']:
+                self.turn_right('dt')
+                self.turn_left('dt')
+                self.rotate_x(dt=True)
+
+
+    def rotate_x(self, cw=True, dt=False):
+        '''
+        Rotates the cube with an x move. This is equivalent to the R turn.
+        
+        Parameters:
+        cw - (optional) Whether to do a clockwise rotation or, if cw=False, a
+             counterclockwise rotation
+        dt - (optional) If dt=True, the cube will have a double turn rotation
+             applied.
+        '''
+        if dt:
+            self.perm = self.perm[[5, 1, 4, 3, 2, 0]]
+            self._rotate(1, 2, 3, 4, length=4)
+        elif cw:
+            self.perm = self.perm[[2, 1, 5, 3, 0, 4]]
+            self._rotate(1, length=-2)
+            self._rotate(3, length=2)
+            self._rotate(4, 5, length=4)
+        else:
+            self.perm = self.perm[[4, 1, 0, 3, 5, 2]]
+            self._rotate(1, length=2)
+            self._rotate(3, length=-2)
+            self._rotate(0, 4, length=4)
+
+
+    def rotate_y(self, cw=True, dt=False):
+        '''
+        Rotates the cube with an y move. This is equivalent to the U turn.
+        
+        Parameters:
+        cw - (optional) Whether to do a clockwise rotation or, if cw=False, a
+             counterclockwise rotation
+        dt - (optional) If dt=True, the cube will have a double turn rotation
+             applied.
+        '''
+        if dt:
+            self.perm = self.perm[[0, 3, 4, 1, 2, 5]]
+            self._rotate(0, 5, length=4)
+        elif cw:
+            self.perm = self.perm[[0, 2, 3, 4, 1, 5]]
+            self._rotate(0, length=2)
+            self._rotate(5, length=-2)
+        else:
+            self.perm = self.perm[[0, 4, 1, 2, 3, 5]]
+            self._rotate(0, length=-2)
+            self._rotate(5, length=2)
+
+
+    def rotate_z(self, cw=True, dt=False):
+        '''
+        Rotates the cube with an z move. This is equivalent to the F turn.
+        
+        Parameters:
+        cw - (optional) Whether to do a clockwise rotation or, if cw=False, a
+             counterclockwise rotation
+        dt - (optional) If dt=True, the cube will have a double turn rotation
+             applied.
+        '''
+        if dt:
+            self.perm = self.perm[[5, 3, 2, 1, 4, 0]]
+            self._rotate(0, 1, 2, 3, 4, 5, length=4)
+        elif cw:
+            self.perm = self.perm[[1, 5, 2, 0, 4, 3]]
+            self._rotate(0, 1, 2, 3, 5, length=2)
+            self._rotate(4, length=-2)
+        else:
+            self.perm = self.perm[[3, 0, 2, 5, 4, 1]]
+            self._rotate(0, 1, 2, 3, 5, length=-2)
+            self._rotate(4, length=2)
 
 
     def apply_alg(self, alg, alg_input=False):
@@ -88,120 +316,5 @@ class Cube:
             alg = alg_to_code(alg)
 
         for turn in alg:
-            turn_info = turn_dict[turn]
-            if turn_info[0] in ['x', 'y', 'z']:
-                self.rotate_cube(*turn_info)
-            else:
-                self.turn_rotate(*turn_info)
-
-
-    def rotate_cube(self, axis, ttype):
-        '''
-        Does a rotation of the cube
-
-        Parameters:
-        axis - The axis of the cube to do the rotation, either x, y or z
-               with x being rotation like the R face, y is like rotation of
-               the U face and z is like the rotation of the F face
-        ttype - The type of rotation, either 1 for a clockwise turn, -1
-                for a counterclockwise turn or 2 for a double turn
-        '''
-        ## An x rotation is just a combination of y and z rotations
-        if axis == 'x':
-            self.rotate_cube('y', 1)
-            self.rotate_cube('z', ttype)
-            self.rotate_cube('y', -1)
-            return
-        ## Setup the correct faces to switch and rotate
-        elif axis == 'y':
-            face_switch = [[0, 2, 3, 4, 1, 5],
-                           [0, 3, 4, 1, 2, 5],
-                           [0, 4, 1, 2, 3, 5]]
-            dt_rotate = [0, 5]
-            st_rotate = [[0], [5]]
-        elif axis == 'z':
-            face_switch = [[1, 5, 2, 0, 4, 3],
-                           [5, 3, 2, 1, 4, 0],
-                           [3, 0, 2, 5, 4, 1]]
-            dt_rotate = [0, 1, 2, 3, 4, 5]
-            st_rotate = [[0, 1, 2, 3, 5], [4]]
-
-        ## Use lists from above to do the switching and string rotations
-        if ttype == 2 or ttype == -2:
-            self.perm = self.perm[face_switch[1]]
-            self._rotate(*dt_rotate, length=4)
-        elif ttype == 1:
-            self.perm = self.perm[face_switch[0]]
-            self._rotate(*st_rotate[0], length=2)
-            self._rotate(*st_rotate[1], length=-2)
-        else:
-            self.perm = self.perm[face_switch[2]]
-            self._rotate(*st_rotate[1], length=2)
-            self._rotate(*st_rotate[0], length=-2)
-
-
-    def turn_rotate(self, face, ttype, dl=False):
-        '''
-        Does the rotation necessary to rotate face to the up face to do a turn
-        by using self.turn.
-
-        Parameters:
-        face - The face to rotate, either 'u', 'l', 'f', 'r', 'b' or 'd'
-        ttype - The type of turn, either 1 for clockwise, -1 for 
-                counterclockwise or 2 for a double turn
-        dl - (optional) If true a double layer turn will be done
-        '''
-        ## Rotate and then turn opposite face for a dl turn
-        if dl:
-            ## Get correct axis to rotate and type of rotation based on face
-            rotation, direction = face_to_rotation(face, True)
-            
-            self.turn_rotate(opposite(face, 'faces'), ttype)
-            self.rotate_cube(rotation, ttype * direction)
-        ## Middle slice is a special case
-        elif face == 'm':
-            self.turn_rotate('r', ttype)
-            self.turn_rotate('l', -ttype)
-            self.rotate_cube('x', -ttype)
-        else:
-            ## Get correct axis to rotate and type of rotation based on face
-            rotation, direction = face_to_rotation(face, False)
-
-            ## Since turns are done to up face, up and down faces are special
-            if face == 'u':
-                self.turn(ttype)
-            elif face == 'd':
-                self.rotate_cube('x', 2)
-                self.turn(ttype)
-                self.rotate_cube('x', 2)
-            else:
-                self.rotate_cube(rotation, direction)
-                self.turn(ttype)
-                self.rotate_cube(rotation, -direction)
-
-
-    def turn(self, ttype):
-        '''
-        Does a turn of the up face
-        
-        Parameters:
-        ttype - The type of turn, either 1 for clockwise, -1 for 
-                counterclockwise or 2 for a double turn
-        '''
-        ## Check if correct ttype
-        turn = (ttype in [1, 'cw']) - (ttype in [-1, 'ccw']) + 2 * (ttype in [2, -2, 'dt'])
-        if turn not in [-1, 1, 2]:
-            raise Exception('ttype = %s, turn amount is %d which is not allowed.'
-                            % (ttype, turn))
-
-        perm_sides = self.perm[1:5]
-        new_perm = []
-
-        ## Rotate the first three stickers of the side faces
-        for n, side in enumerate(perm_sides):
-            next_side = (n + turn) % 4
-            new_perm.append(perm_sides[next_side][:3] + perm_sides[n][3:])
-
-        self.perm[[1, 2, 3, 4]] = new_perm
-        ## Rotate top face stickers
-        self._rotate(0, length=2 * turn)
+            method = turn_dict[turn]
+            getattr(self, method[0])(method[1], method[2])
