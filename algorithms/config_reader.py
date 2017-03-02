6cf639897650f2115fa2b9a31b20fff4ece284cf
Jacob Scott <jscott12009@gmail.com>
2017-03-02 15:22:16 -0800

    Add comments

--- algorithms/config_reader.py
+++ algorithms/config_reader.py
@@ -1,5 +1,10 @@
+'''
+Reads the dicts from alg_config.py and converts the algorithms into the syntax
+used by the code.
+'''
 from algorithms.alg_config import oll_algs, pll_algs
 from algorithms.tools import alg_to_code
+
 
 pll_code = {}
 for name, alg in pll_algs.items():
